ed38e58678e2f94de3891b7cbb0f1cd080ae5034
Marcio Rela <marciorela75@gmail.com>
2020-10-23 12:05:03 -0300

    Exercicio: Arvore Genealógica

---
+++ ArvoreGenealogica/ArvoreGenealogica/Pessoa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArvoreGenealogica
+{
+    public class Pessoa
+    {
+        public Pessoa(string nome, Pessoa conjuge = null)
+        {
+            Nome = nome;
+            Conjuge = conjuge;
+
+            Filhos = new List<Pessoa>();
+        }
+
+        public void AddFilho(Pessoa filho)
+        {
+            Filhos.Add(filho);
+        }
+
+        public void Imprimir(int nivel = 0)
+        {
+            Console.Write($"{new String(' ', nivel * 4)} -->{Nome} ");
+            if (Conjuge == null)
+            {
+                Console.WriteLine("é solteiro(a).");
+            } 
+            else
+            {
+                Console.WriteLine($"é casado(a) com {Conjuge.Nome} filhos:");
+                foreach (var filho in Filhos)
+                {
+                    filho.Imprimir(nivel+1);
+                }
+            }
+        }
+
+        public string Nome { get; set; }
+        public Pessoa Conjuge { get; set; }
+
+        public IList<Pessoa> Filhos { get; set; }
+    }
+}
---
+++ ArvoreGenealogica/ArvoreGenealogica/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ArvoreGenealogica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lily = new Pessoa("Lily", new Pessoa("Wilhelm"));
+            var opa = new Pessoa("Opa", new Pessoa("Oma"));
+            var reinhold = new Pessoa("Reinhold", new Pessoa("Sonia"));
+            var christian = new Pessoa("Christian", new Pessoa("Mônica"));
+            var oscar = new Pessoa("Oscar");
+            var lorena = new Pessoa("Lorena");
+            var gabriel = new Pessoa("Gabriel");
+            var sabine = new Pessoa("Sabine");
+
+            var wilma = new Pessoa("Wilma", new Pessoa("Rodolfo"));
+            var ricardo = new Pessoa("Ricardo", new Pessoa("Debora"));
+            var rodrigo = new Pessoa("Rodrigo");
+
+            var sigrid = new Pessoa("Sigrid", new Pessoa("Peter"));
+            var martin = new Pessoa("Martin", new Pessoa("Carla"));
+
+            var nicolas = new Pessoa("Nicolas");
+            var thomas = new Pessoa("Thomas");
+            var claudia = new Pessoa("Claudia");
+
+            lily.AddFilho(opa);
+
+            opa.AddFilho(reinhold);
+            opa.AddFilho(wilma);
+            opa.AddFilho(sigrid);
+
+            reinhold.AddFilho(christian);
+            reinhold.AddFilho(gabriel);
+            reinhold.AddFilho(sabine);
+
+            christian.AddFilho(oscar);
+            christian.AddFilho(lorena);
+
+            wilma.AddFilho(ricardo);
+            wilma.AddFilho(rodrigo);
+
+            sigrid.AddFilho(martin);
+            sigrid.AddFilho(thomas);
+            sigrid.AddFilho(claudia);
+
+            martin.AddFilho(nicolas);
+
+            lily.Imprimir();
+        }
+    }
+}
